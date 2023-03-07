210fcc2fd03603ec3802ed3c02537c1944fb1fb1
Jackson Holliday <jaxholliday@gmail.com>
2023-03-07 08:36:53 -0500

    Update site.js

--- js/site.js
+++ js/site.js
@@ -1,2 +1,41 @@
 //Get values from the page from the user
 //fizz and buzz value
+function getValues(){
+
+    //get user values from page
+
+    //Parse the numbers
+
+    //check to make sure numsare integers
+
+    //we can call fizzbuzz function
+
+    //call of display data function
+}
+
+function fizzBuzz(fizzValue, buzzValue){
+
+    //initialize an array
+    let returnArray = [];
+
+    //loop for the fizzbuzz ==> 1 to 100
+
+    //we need to check the current in 3 steps
+    //check to see is divisible bu both ==> fizzBuzz (3 and 5) then push to array    
+    
+    //check to see is divisible bu both ==> fizz(3) then push to array
+
+
+    //check to see is divisible bu both ==> Buzz(5) then push to array
+
+
+    //if neither then number is pushed to array
+
+    returnArray;
+}
+
+function displayData(fbArray){
+
+    //loop over the array and print them to the screen
+
+}
