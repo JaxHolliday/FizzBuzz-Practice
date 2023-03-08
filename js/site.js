ad1bedc0877594dceea6dca2ff78601599a34fc6
Jackson Holliday <jaxholliday@gmail.com>
2023-03-08 09:49:07 -0500

    Update site.js

--- js/site.js
+++ js/site.js
@@ -3,34 +3,43 @@
 function getValues(){
 
     //get user values from page
+    let fizzValue = document.getElementById("fizzValue").value;
+    let buzzValue = document.getElementById("buzzValue").value;
 
     //Parse the numbers
+    fizzValue = parseInt(fizzValue);
+    buzzValue = parseInt(buzzValue);
 
     //check to make sure numsare integers
+    if (Number.isInteger(fizzValue) && Number.isInteger(buzzValue)) {
 
     //we can call fizzbuzz function
-
+        let fbArray = fizzBuzz(fizzValue, buzzValue);
     //call of display data function
+        displayData(fbArray);
+    } else {
+        alert("You must enter integers")
+    }
 }
 
+//logic function
 function fizzBuzz(fizzValue, buzzValue){
-
     //initialize an array
     let returnArray = [];
 
     //loop for the fizzbuzz ==> 1 to 100
 
-    //we need to check the current in 3 steps
-    //check to see is divisible bu both ==> fizzBuzz (3 and 5) then push to array    
-    
-    //check to see is divisible bu both ==> fizz(3) then push to array
-
-
-    //check to see is divisible bu both ==> Buzz(5) then push to array
-
-
-    //if neither then number is pushed to array
-
+    for (let i = 1; i <= 100; i++) {
+        if (i % fizzValue == 0 && i % buzzValue == 0) {
+            returnArray.push('FizzBuzz')
+        } else if (i % fizzValue == 0){ 
+            returnArray.push('Fizz')
+        } else if (i % buzzValue == 0) {
+            returnArray.push('Buzz')
+        } else {
+            returnArray.push(i);
+        }        
+    }
     returnArray;
 }
 
