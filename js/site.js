da2d66537dcef7a9c2e363b183c01bb23e59925e
Jackson Holliday <jaxholliday@gmail.com>
2023-03-12 17:37:46 -0400

    css updates

--- js/site.js
+++ js/site.js
@@ -61,11 +61,11 @@ function displayData(fbArray){
 
         //grab use td to put in array
         let rowCols = tableRow.querySelectorAll("td");
-        rowCols[0].textContent = fbData[i];
-        rowCols[1].textContent = fbData[i + 1];
-        rowCols[2].textContent = fbData[i + 2];
-        rowCols[3].textContent = fbData[i + 3];
-        rowCols[4].textContent = fbData[i + 4];
+        rowCols[0].textContent = fbArray[index];
+        rowCols[1].textContent = fbArray[index + 1];
+        rowCols[2].textContent = fbArray[index + 2];
+        rowCols[3].textContent = fbArray[index + 3];
+        rowCols[4].textContent = fbArray[index + 4];
 
         tableBody.appendChild(tableRow);
 
