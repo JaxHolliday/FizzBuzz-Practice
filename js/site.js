f83b8e089e5d4bd691140786c355355922887dc9
Jackson Holliday <jaxholliday@gmail.com>
2023-03-12 17:45:27 -0400

    js display data update

--- js/site.js
+++ js/site.js
@@ -1,6 +1,6 @@
 //Get values from the page from the user
 //fizz and buzz value
-function getValues(){
+function getValues() {
 
     //get user values from page
     let fizzValue = document.getElementById("fizzValue").value;
@@ -13,9 +13,9 @@ function getValues(){
     //check to make sure numsare integers
     if (Number.isInteger(fizzValue) && Number.isInteger(buzzValue)) {
 
-    //we can call fizzbuzz function
+        //we can call fizzbuzz function
         let fbArray = fizzBuzz(fizzValue, buzzValue);
-    //call of display data function
+        //call of display data function
         displayData(fbArray);
     } else {
         alert("You must enter integers")
@@ -23,7 +23,7 @@ function getValues(){
 }
 
 //logic function
-function fizzBuzz(fizzValue, buzzValue){
+function fizzBuzz(fizzValue, buzzValue) {
     //initialize an array
     let returnArray = [];
 
@@ -32,18 +32,18 @@ function fizzBuzz(fizzValue, buzzValue){
     for (let i = 1; i <= 100; i++) {
         if (i % fizzValue == 0 && i % buzzValue == 0) {
             returnArray.push('FizzBuzz')
-        } else if (i % fizzValue == 0){ 
+        } else if (i % fizzValue == 0) {
             returnArray.push('Fizz')
         } else if (i % buzzValue == 0) {
             returnArray.push('Buzz')
         } else {
             returnArray.push(i);
-        }        
+        }
     }
     return returnArray;
 }
 
-function displayData(fbArray){
+function displayData(fbArray) {
 
     //get the table body element from page
     let tableBody = document.getElementById("results");
@@ -56,15 +56,24 @@ function displayData(fbArray){
 
     //loop is adding 5 for every increment instead of 1
     for (let index = 0; index < fbArray.length; index += 5) {
-        
+
         let tableRow = document.importNode(templateRow.content, true);
 
         //grab use td to put in array
         let rowCols = tableRow.querySelectorAll("td");
+        rowCols[0].classList.add(fbArray[index]);
         rowCols[0].textContent = fbArray[index];
+
+        rowCols[1].classList.add(fbArray[index + 1]);
         rowCols[1].textContent = fbArray[index + 1];
+
+        rowCols[2].classList.add(fbArray[index + 2]);
         rowCols[2].textContent = fbArray[index + 2];
+
+        rowCols[3].classList.add(fbArray[index + 3]);
         rowCols[3].textContent = fbArray[index + 3];
+
+        rowCols[4].classList.add(fbArray[index + 4]);
         rowCols[4].textContent = fbArray[index + 4];
 
         tableBody.appendChild(tableRow);
